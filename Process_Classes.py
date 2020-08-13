4b84db21e0e44d8f5ad0a00348e84f8c9e6d29a3
Berke Atalay <berkeatalay@outlook.com.tr>
2020-08-13 13:18:09 +0300

    HM4

--- Process_Classes.py
+++ Process_Classes.py
@@ -160,30 +160,31 @@ class models:
         else:
             x = self.x
         
+      
+             
+      
+        X_pca = pd.DataFrame(self.classifier.fit_transform(x),index = x.index)
+        
         if split:
             from sklearn.model_selection import train_test_split
-            X_train, X_test, y_train, y_test = train_test_split(x, self.y, test_size = rate, random_state = rs)
+            X_train, X_test, y_train, y_test = train_test_split(X_pca, self.y, test_size = rate, random_state = rs)
             self.X_train = X_train
             self.y_train = y_train
             self.X_test = X_test
             self.y_test = y_test
         
         else:
-            X_train = x
-            self.X_train = x
+            X_train = X_pca
+            self.X_train = X_pca
             self.y_train = self.y
-            self.X_test = x
-            y_train = self.y
+            self.X_test = X_pca
+            self.y_train = self.y
             self.y_test  = self.y
-        
-        
-        
-        
-        X_pca = self.classifier.fit_transform(self.X_train)
+            
         arr = np.cumsum(np.round(self.classifier.explained_variance_ratio_, decimals = 4)*100)
         num_var = sum((arr < threshold*100)) + 1 
         print('Pca sonrası değişken sayısı: ',num_var)
-        X_pcad = pd.DataFrame(X_pca[:,0:num_var], index = self.X_train.index)
+        X_pcad = self.X_train[:,0:num_var]
         self.X_train_pcad = X_pcad
         print("New X_train values are saved as X_train_pcad")
         
@@ -273,15 +274,114 @@ class models:
                 y_pred = [1 if i > imp[j][1] else 0 for i in y_probs]
                 print(classification_report(self.y_test,y_pred))
             
-            print("For test data and with max f1 score:")
+            print("For test data and with max f1 score: using prop thshold as: ",imp[0][1])
             y_pred = [1 if i > imp[0][1] else 0 for i in y_probs]
             print(confusion_matrix(self.y_test,y_pred))
-            print("For train data and with max f1 score:")
+            print("For train data and with max f1 score:using prop thshold as: ",imp[0][1])
             y_probs = self.classifier.predict_proba(self.X_train)[:,1]
             y_pred = [1 if i > imp[0][1] else 0 for i in y_probs]
             print(confusion_matrix(self.y_train,y_pred))
             
         
+        
+class data_classification():
+    
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        print("Classification class")
+        
+    def split(self, rs=0):
+        from sklearn.model_selection import train_test_split
+        if rs:
+            X_train, X_test, y_train , y_test = train_test_split(self.x,self.y,test_size = 0.3, random_state=rs)
+        else:
+            X_train, X_test, y_train , y_test = train_test_split(self.x,self.y,test_size = 0.3)
+            
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+    
+    def default_processes(self):
+        
+        from sklearn.tree import DecisionTreeClassifier
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.neural_network import MLPClassifier
+        from sklearn.metrics import classification_report
+        
+        dtc = DecisionTreeClassifier()
+        dtc.fit(self.X_train,self.y_train)
+        y_pred = dtc.predict(self.X_test)
+        print('Results for default decision tree')
+        print(classification_report(self.y_test,y_pred))
+        
+        rfc = RandomForestClassifier()
+        rfc.fit(self.X_train,self.y_train)
+        y_pred = rfc.predict(self.X_test)
+        print('Results for default random forest')
+        print(classification_report(self.y_test,y_pred))
+        
+        mlpc = MLPClassifier()
+        mlpc.fit(self.X_train,self.y_train)
+        y_pred = mlpc.predict(self.X_test)
+        print('Results for default MLP')
+        print(classification_report(self.y_test,y_pred))
+        
+    def grid(self,method, params = {}):
+        from sklearn.model_selection import  GridSearchCV
+        if params:
+            if method == 'rf':
+                from sklearn.ensemble import RandomForestClassifier
+                classifier = RandomForestClassifier()
+                grid_params=params
+
+            elif method == 'dt':
+                from sklearn.tree import DecisionTreeClassifier
+                classifier = DecisionTreeClassifier()
+                grid_params=params
+
+            elif method == 'mlp':
+                from sklearn.neural_network import MLPClassifier
+                classifier = MLPClassifier()
+                grid_params=params
+
+            else:
+                print('Unknown method')
+                return
+        else:
+            if method == 'rf':
+                from sklearn.ensemble import RandomForestClassifier
+                classifier = RandomForestClassifier()
+                grid_params = {"max_depth": [8,10,11,13,15,18],
+                    "max_features": [5,10,15,20],
+                     "n_estimators": [5,10,50,100,200,500],
+                     "min_samples_split": [3,5,10],
+                    "criterion":['entropy','gini']}
+
+            elif method == 'dt':
+                from sklearn.tree import DecisionTreeClassifier
+                classifier = DecisionTreeClassifier()
+                grid_params = {"max_depth": range(1,10),
+                    "min_samples_split": list(range(2,50)),
+                    "criterion":['gini', 'entropy']}  
+
+            elif method == 'mlp':
+                from sklearn.neural_network import MLPClassifier
+                classifier = MLPClassifier()
+                grid_params = {
+                    'alpha':[0.1,0.01,0.001,0.005,0.0001,0.00001],
+                    'hidden_layer_sizes': [(10,10,10),(45,50,60),(25,35,45),(15,15)],
+                    'solver': ['lbfgs','adam','sgd'],
+                    'activation': ['relu','logistic','tanh','identity']  }
+            else:
+                print('Unknown method')
+                return
+
+        grid_cv = GridSearchCV(classifier, grid_params, cv=10, n_jobs=-1, verbose=2)
+        grid_cv_model = grid_cv.fit(self.X_train,self.y_train)
+        
+        print("En iyi parametlerler: " + str(grid_cv_model.best_params_))
         
         
         
